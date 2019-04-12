740d3ad9267ef91afe610de53624b42b0a9b9a6a
trik <marco.marche@gnucoop.com>
2019-04-12 11:47:54 +0200

    fix(angular): missing value accessor for autocomplete input

---
+++ angular/src/directives/control-value-accessors/autocomplete-value-accessor.ts
@@ -0,0 +1,26 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+import { ɵb as ValueAccessor } from '@ionic/angular';
+
+@Directive({
+  /* tslint:disable-next-line:directive-selector */
+  selector: 'gic-autocomplete',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: AutocompleteValueAccessor,
+      multi: true
+    }
+  ]
+})
+export class AutocompleteValueAccessor extends ValueAccessor {
+
+  constructor(el: ElementRef) {
+    super(el);
+  }
+
+  @HostListener('ionChange', ['$event.target.value'])
+  _handleChangeEvent(value: any) {
+    this.handleChangeEvent(value);
+  }
+}
